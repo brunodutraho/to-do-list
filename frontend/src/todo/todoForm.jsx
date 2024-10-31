00a83a190c4f63dc8d30dac223a2a037a4e1d24b
Bruno Dutra <brunodutra.ho@gmail.com>
2024-10-31 10:59:32 -0300

    Event onChange

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,7 +5,10 @@ import IconButton from '../template/iconButton'
 export default props => (
         <div role='form' className='todoForm d-flex flex-column mb-3'>
         <Grid cols='12 9 10'>
-            <input type="text" id='description' placeholder='Adicione uma tarefa' className="form-control" />
+            <input type="text" id='description' placeholder='Adicione uma tarefa'
+            value={props.description}
+            onChange={props.handleChange}
+            className="form-control" />
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton icon='plus' style='primary' onClick={props.handleAdd}  />
