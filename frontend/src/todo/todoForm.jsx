6f0871f8bd0adb1f271c0d8acb657b5239628812
Bruno Dutra <brunodutra.ho@gmail.com>
2024-11-21 10:11:04 -0300

    custom css/ clear button

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,15 +4,16 @@ import IconButton from '../template/iconButton'
 
 export default props => (
         <div role='form' className='todoForm d-flex flex-column mb-3'>
-        <Grid cols='12 9 10'>
+        <Grid cols='12 9 10' classGrid=''>
             <input type="text" id='description' placeholder='Adicione uma tarefa'
             value={props.description}
             onChange={props.handleChange}
             className="form-control" />
         </Grid>
-        <Grid cols='12 3 2'>
+        <Grid cols='12 3 2' classGrid=' btn-respo'>
             <IconButton icon='plus' style='primary' onClick={props.handleAdd}  />
             <IconButton icon='search' style='info' onClick={props.handleSearch}  />
+            <IconButton icon='close' style='danger' onClick={props.handleClear}  />
         </Grid>
     </div>
 )
