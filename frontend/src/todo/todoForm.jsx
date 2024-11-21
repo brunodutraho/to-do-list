0fc0624debc63af934fe6b5429593685be23716a
Bruno Dutra <brunodutra.ho@gmail.com>
2024-11-21 10:21:13 -0300

    shortcut keys

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
+export default props => {
+
+    const keyHandler = (e) => {
+        if(e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if(e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+
+    return (
         <div role='form' className='todoForm d-flex flex-column mb-3'>
         <Grid cols='12 9 10' classGrid=''>
             <input type="text" id='description' placeholder='Adicione uma tarefa'
             value={props.description}
             onChange={props.handleChange}
+            onKeyUp={keyHandler}
             className="form-control" />
         </Grid>
         <Grid cols='12 3 2' classGrid=' btn-respo'>
@@ -16,4 +27,4 @@ export default props => (
             <IconButton icon='close' style='danger' onClick={props.handleClear}  />
         </Grid>
     </div>
-)
+)}
